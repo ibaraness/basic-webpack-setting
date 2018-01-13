aa619115a072c43a0fc704c97fd4393db0c0ff27
ibaraness <idan.baraness@gmail.com>
2018-01-13 16:53:28 +0200

    added comments

--- webpack.config.js
+++ webpack.config.js
@@ -12,27 +12,42 @@ module.exports = {
                 /* Path.resolve returns an absolute path of a list of directories.  
                    __dirname gives us the name of the current directory  */
                 path: path.resolve(__dirname,'dist'),
+
                 /* Create scripts that will be loaded dynamically */
                 chunkFilename: '[name].bundle.js',
+
                 /* Use the name declared in the entry object */
                 filename: '[name].js'
         },
         module: {
             rules: [
                 {
+                    /* Set webpack loaders to handle SASS scss files */
                     test: /\.scss$/,
+
+                    /* Here we are using a special plugin to let us extract our styles to different files */
                     use: ExtractTextWebpackPlugin.extract({
+
+                        /* If something goes wrong, load css with the style loader */
                         fallback: "style-loader",
+
+                        /* Here we tell the plugin what loaders to use in order to create CSS files */
                         use: ["css-loader","sass-loader"]
                     })
                 },
                 {
+                    /* Set webpack loaders to handle image and vector files */
                     test:/\.(png|svg|jpg|gif)$/,
                     use: [
                         {
                             loader: "file-loader",
+                            /*
+                             * Here we set loader options 
+                             */
                             options:{
+                                /* We tell the loader to put the images in '/images'/ directory */
                                 outputPath:'./images/',
+                                /* We tell the loader to use the original image name and extension */
                                 name:'[name].[ext]'
                             }
                         }
@@ -41,8 +56,13 @@ module.exports = {
             ]
         },
         plugins: [
+            /* Clean the dist directory before each buid */
             new CleanWebpackPlugin('./dist'),
+
+            /* Extract all CSS to "styles.css" file and crete link element on header */
             new ExtractTextWebpackPlugin("styles.css"),
+
+            /* Prepare HTML file using a template with our bundle files */
             new HtmlWebpackPlugin({
                 template:'./src/index.html',
                 inject:'body'
